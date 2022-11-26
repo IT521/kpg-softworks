a810eefb96a739a95b9f00c5fc962a3ca0039d58
IT521 <terrysfrancis@hotmail.com>
2022-11-25 19:50:16 -0500

    fix: move router to entry file

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import './App.scss';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { About, Home, Layout, NoMatch } from './pages';
 
 export default function App() {
     return (
-        <Router>
-            <div className="main">
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Home />} />
-                        <Route path="about" element={<About />} />
-                        <Route path="*" element={<NoMatch />} />
-                    </Route>
-                </Routes>
-            </div>
-        </Router>
+        <div className="main">
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="*" element={<NoMatch />} />
+                </Route>
+            </Routes>
+        </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import './index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // @ts-ignore
 import App from './App.tsx';
@@ -14,7 +15,9 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>
 );
 
